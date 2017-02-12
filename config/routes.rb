64880946c5691ad63d2c6bbbd8c7ea2e81b12715
Jamie Menhall <jamiemenhall@mac.com>
2017-02-12 16:55:10 -0500

    made pages for each one and usage of id number to get to each page using params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#home_page'
   get 'restaurants', to:'restaurants#index'
+  get 'restaurants/:id', to: 'restaurants#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
