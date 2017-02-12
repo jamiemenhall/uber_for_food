8d0ea55d1182281885c9fc10add101568b163633
Jamie Menhall <jamiemenhall@mac.com>
2017-02-12 16:57:10 -0500

    changed routing to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#home_page'
-  get 'restaurants', to:'restaurants#index'
-  get 'restaurants/:id', to: 'restaurants#show'
+  # get 'restaurants', to:'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
